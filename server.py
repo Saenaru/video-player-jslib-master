244cf69932d0481f42d8a2de435b444fa09b7f80
Saenaru <Nyanja230@gmail.com>
2025-05-10 01:04:28 +0300

    add icon to play button

---
+++ server.py
@@ -0,0 +1,14 @@
+from livereload import Server
+
+def rebuild():
+    print("Files changed - reloading...")
+
+server = Server()
+
+# Watch all relevant files for changes
+server.watch('index.html', rebuild)
+server.watch('player.js', rebuild)
+server.watch('styles.css', rebuild)
+
+# Serve the application from current directory
+server.serve(root='.', host='0.0.0.0', port=5500)
